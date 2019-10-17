d01d5975954c32aaae007a9fd0f733d0d38fbc09
mhjabrane <mohamed.jabrane@adservio.fr>
2019-10-17 18:24:55 +0200

    Initial commit

---
+++ src/main/java/guru/sfg/brewery/bootstrap/DefaultBreweryLoader.java
@@ -0,0 +1,108 @@
+/*
+ *  Copyright 2019 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package guru.sfg.brewery.bootstrap;
+
+import guru.sfg.brewery.domain.Beer;
+import guru.sfg.brewery.domain.Brewery;
+import guru.sfg.brewery.domain.Customer;
+import guru.sfg.brewery.repositories.BeerRepository;
+import guru.sfg.brewery.repositories.BreweryRepository;
+import guru.sfg.brewery.repositories.CustomerRepository;
+import guru.sfg.brewery.web.model.BeerStyleEnum;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+
+/**
+ * Created by jt on 2019-01-26.
+ */
+@Component
+public class DefaultBreweryLoader implements CommandLineRunner {
+
+    public static final String TASTING_ROOM = "Tasting Room";
+    public static final String BEER_1_UPC = "0631234200036";
+    public static final String BEER_2_UPC = "0631234300019";
+    public static final String BEER_3_UPC = "0083783375213";
+    private final BreweryRepository breweryRepository;
+    private final BeerRepository beerRepository;
+    private final CustomerRepository customerRepository;
+
+
+    public DefaultBreweryLoader(BreweryRepository breweryRepository,
+                                BeerRepository beerRepository, CustomerRepository customerRepository) {
+        this.breweryRepository = breweryRepository;
+        this.beerRepository = beerRepository;
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBreweryData();
+        loadCustomerData();
+    }
+
+    private void loadCustomerData() {
+        if (customerRepository.count() == 0) {
+            customerRepository.save(Customer.builder()
+                    .customerName(TASTING_ROOM)
+                    .apiKey(UUID.randomUUID())
+                    .build());
+        }
+    }
+
+    private void loadBreweryData() {
+        if (breweryRepository.count() == 0){
+            breweryRepository.save(Brewery
+                    .builder()
+                    .breweryName("Cage Brewing")
+                    .build());
+
+            Beer mangoBobs = Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle(BeerStyleEnum.IPA)
+                    .minOnHand(12)
+                    .quantityToBrew(200)
+                    .upc(BEER_1_UPC)
+                    .build();
+
+            beerRepository.save(mangoBobs);
+
+            Beer galaxyCat = Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle(BeerStyleEnum.PALE_ALE)
+                    .minOnHand(12)
+                    .quantityToBrew(200)
+                    .upc(BEER_2_UPC)
+                    .build();
+
+            beerRepository.save(galaxyCat);
+
+            Beer pinball = Beer.builder()
+                    .beerName("Pinball Porter")
+                    .beerStyle(BeerStyleEnum.PORTER)
+                    .minOnHand(12)
+                    .quantityToBrew(200)
+                    .upc(BEER_3_UPC)
+                    .build();
+
+            beerRepository.save(pinball);
+
+        }
+    }
+}
